68f0450acacfe22ec47d94f4a831df3bcd7b1b19
Abhiram-python <gurujalaabhiram@gmail.com>
2025-08-28 02:58:24 +0530

    got

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from flask import Flask,render_template
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
+upd_val="20F"
+
 @app.route("/")
 def home():
     return render_template("main.html")
+
+@app.route("/update")
+def update():
+    return upd_val
