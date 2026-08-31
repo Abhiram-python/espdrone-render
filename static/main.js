0452dc255c2cd77b445f6d3e79a513845f3f03a9
Abhiram-python <gurujalaabhiram@gmail.com>
2026-09-01 02:49:52 +0530

    g

--- static/main.js
+++ static/main.js
@@ -40,7 +40,7 @@ document.addEventListener("pointermove", (ev) => {
 
         console.log(ev.clientY - ojoy_oy)
 
-        remotetest(ev.clientY-ojoy_oy + topV)
+        remotetest(ev.clientY-ojoy_oy + topv)
     }
 
     else if (ev.pointerId === fjoy_pointerId) {
